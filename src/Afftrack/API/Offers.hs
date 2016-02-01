d9d132397293a92a32899e7e64a806b330367cc3
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-02-01 10:30:52 +0200

    update offers methods and cabal dependencies

--- src/Afftrack/API/Offers.hs
+++ src/Afftrack/API/Offers.hs
@@ -1,1 +1,79 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
 module Afftrack.API.Offers where
+
+import GHC.Generics
+import Data.Aeson
+import Control.Applicative
+import Network.HTTP.Client
+import qualified Data.ByteString.Char8 as BS
+
+--------------------------------------------------------------------------------
+
+data Resp =
+  Resp { datas   :: [Offer]
+       , success :: Bool
+       , page    :: Int
+       , total   :: Int
+       , url     :: String
+       , pages   :: Int
+       , limit   :: Int  
+       } deriving (Generic, Show)
+
+instance FromJSON Resp where
+  parseJSON (Object v) =
+    Resp <$> v .: "data"       <*>
+             v .: "success"    <*>
+             v .: "page"       <*>
+             v .: "total"      <*>
+             v .: "request_url"<*>
+             v .: "total_pages"<*>
+             v .: "limit"
+    -- A non-Object value is of the wrong type, so fail.
+  parseJSON _        = empty  
+
+data TrafficType = INCENT | NONINCENT
+
+data Offer =
+  Offer { name       :: String
+        , link       :: String
+        , linkStatus :: Int
+        , payout     :: Float
+        , merchantID :: Int  
+        } deriving (Generic, Show)
+
+instance FromJSON Offer where
+  parseJSON (Object v) =
+    Offer <$> v .: "program_name"         <*>
+              v .: "program_preview_link" <*>
+              v .: "program_link_status"  <*>
+              v .: "program_adv_paying"   <*>
+              v .: "program_mid"
+    -- A non-Object value is of the wrong type, so fail.
+  parseJSON _        = empty  
+
+callOffers = Call "offer_offer"
+                  "getOffer"
+                  "GET"
+                  [ ("category", "")
+                  , ("converts_on", "")
+                  , ("device_type", "")
+                  , ("limit", "")
+                  , ("merchant_id", "") -- empty
+                  , ("name", "")
+                  , ("offer_id", "")
+                  , ("offer_type", "")
+                  , ("orderby", "")
+                  , ("page", "")
+                  , ("sort", "")
+                  , ("status", "107")
+                  , ("tracking_type", "")
+                  , ("traffic_type", "")
+                  ]
+
+callOfferStatuses = Call "offer_offer"
+                         "getOfferStatus"
+                         "GET"
+                         []
