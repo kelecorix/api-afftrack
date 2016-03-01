cb5af03a775f3a4ac311058940acce3522762582
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-03-01 09:22:35 +0200

    update Merchant target methods

--- src/Afftrack/API/Merchant.hs
+++ src/Afftrack/API/Merchant.hs
@@ -1,1 +1,23 @@
-module Afftrack.API.Merchant where
+module Afftrack.API.Merchant
+       ( addBrowserLanguageBlocked
+       )
+       where
+
+import GHC.Generics
+import Data.Aeson
+import Control.Applicative
+import Network.HTTP.Client
+import qualified Data.ByteString.Char8 as BS
+
+import Afftrack.API.Common
+
+--------------------------------------------------------------------------------
+
+addBrowserLanguageBlocked = 
+  Call "mer_merchant"
+       "addMerchantBlacklist"
+       "POST"
+       [ Param "affiliate_id" True ""   -- Required
+       , Param "merchant_id"  True ""   -- Required
+       ]
+
