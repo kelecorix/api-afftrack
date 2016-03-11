98eac6326972af692ecf23fea08e67071e60b25a
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-03-11 11:00:16 +0200

    update Types

--- src/Afftrack/API/Types.hs
+++ src/Afftrack/API/Types.hs
@@ -1,10 +1,49 @@
-module Afftrack.API.Types where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
 
-import -- Aeson
+module Afftrack.API.Types
+       ( Auth(..)
+       , Resp(..)
+       , Offer(..)  
+       ) where
 
-type Offer =
-  Offer { program_pid :: Int
-        , program_mid :: Int
-        , program_name:: Text
-        , program_name_private::Int  
-        }
+import GHC.Generics
+import Data.Aeson
+import qualified Data.Text as T
+import Control.Applicative
+
+-----------------------------------------------------------------------------
+
+-- | Core parameters that builds up
+--   auth url to acces methods
+-- 
+--   endpoint = "http://tracking.affiliate.net/apiv4/"
+--   key      = "9bb137a344cdec76c5830a0ef6d2e38e"
+--   api      =  endpoint ++ "?key=" ++ key ++ "&format=json"
+data Auth =
+  Auth { endpoint :: T.Text
+       , key      :: T.Text
+       , format   :: T.Text  
+       } deriving (Show)
+
+data Offer =
+  Offer { name       :: T.Text
+        , link       :: T.Text
+        , linkStatus :: Int
+        , payout     :: Float
+        , merchantID :: Int  
+        } deriving (Generic, Show)
+
+instance FromJSON Offer where
+  parseJSON (Object v) =
+    Offer <$> v .: "program_name"         <*>
+              v .: "program_preview_link" <*>
+              v .: "program_link_status"  <*>
+              v .: "program_adv_paying"   <*>
+              v .: "program_mid"
+    -- A non-Object value is of the wrong type, so fail.
+  parseJSON _        = empty  
+
+
