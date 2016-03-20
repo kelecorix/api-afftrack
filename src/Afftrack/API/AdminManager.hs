d49e0ce277179e2ed2dd306c1ba6c1a36fa8fac8
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2016-03-20 15:04:57 +0200

    Update Admin target

--- src/Afftrack/API/AdminManager.hs
+++ src/Afftrack/API/AdminManager.hs
@@ -1,1 +1,33 @@
-module Afftrack.API.AdminManager where
+module Afftrack.API.AdminManager
+       ( getAdmins
+       , getAdminStatus  
+       )where
+
+import GHC.Generics
+import Data.Aeson
+import Control.Applicative
+import Network.HTTP.Client
+import qualified Data.ByteString.Char8 as BS
+import Data.Text
+import Afftrack.API.Common
+import Afftrack.API.Types
+
+--------------------------------------------------------------------------------
+
+getAdmins = 
+  Call "admin_managers"
+       "getAdmins"
+       "GET"
+       [ Param "limit"   False "" 
+       , Param "orderby" False ""
+       , Param "page"    False ""
+       , Param "sort"    False ""
+       , Param "status"  False ""  
+       ]
+
+getAdminStatus =
+  Call "admin_managers"
+       "getAdminStatus"
+       "GET"
+       [
+       ]
